b4a45ae476e11cb8ec47c0b715e8af5311206e58
Victor Anuebunwa <anuebunwa.victor@gmail.com>
2018-12-08 17:09:18 +0100

    Fixes

--- src/liteframe/Database/DBSetup.php
+++ src/liteframe/Database/DBSetup.php
@@ -38,9 +38,10 @@ if (empty($GLOBALS['__rbloaded'])) {
         R::freeze($freezeState);
     }
     
-//    R::ext('xdispense', function ($type) {
-//        return R::getRedBean()->dispense($type);
-//    });
+    //Configure prefix extension
+    R::ext('xdispense', function ($type) {
+        return R::getRedBean()->dispense($type);
+    });
 
     /**
      * True is RedBeanPHP has been loaded, else false
